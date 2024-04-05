e4e3d2b224ad55d02a49e2d859a1612ea09950a1
Bartlomiej Podlewski <podlewski19@gmail.com>
2024-04-05 12:44:55 +0200

    rozwiązanie

---
+++ usersData.js
@@ -0,0 +1,14 @@
+const usersData = [
+    {
+        "name": "Bartłomiej",
+        "email": "bartek@gmail.com",
+        "password": "haslo123"
+    },
+    {
+        "name": "Arturito",
+        "email": "artur@gmail.com",
+        "password": "pasta123"
+    }
+];
+
+module.exports = {usersData};
